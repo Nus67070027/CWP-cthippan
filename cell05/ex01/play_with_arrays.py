bfde7db27332a373b0694d157a01328ceea6ccac
Nus67070027 <67070027@kmitl.ac.th>
2026-09-24 09:54:24 +0700

    1

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,2 @@
+arr: list = [2, 8, 9, 48, 8, 22, -12, 2]
+print("Original array:", arr,"\nNew array:", [i+2 for i in arr])
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,5 @@
+arr: list = [2, 8, 9, 48, 8, 22, -12, 2]
+
+newarr: list = [i+2 for i in arr]
+
+print("Original array:", arr,"\nNew array:", newarr[1:6])
